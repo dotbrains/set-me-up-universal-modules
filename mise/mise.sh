39aa86632ad973208c4b491c2ac57b9762686575
Nicholas Adamou <nicholasadamouemail@gmail.com>
2026-01-16 04:28:53 -0500

    Add brewfile and mise.sh for Homebrew installation

---
+++ mise/mise.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# shellcheck source=/dev/null
+
+declare current_dir &&
+    current_dir="$(dirname "${BASH_SOURCE[0]}")" &&
+    cd "${current_dir}" &&
+    source "$HOME/set-me-up/dotfiles/utilities/utilities.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    ask_for_sudo
+
+    brew_bundle_install -f "brewfile"
+}
